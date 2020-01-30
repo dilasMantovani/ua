33607119c2d25dfe9f4da915768f58795d455a98
Diego Dias <diego.dias@vilt-group.com>
2020-01-30 17:56:07 -0300

    error checking

--- src/main/java/br/com/dilas/ua/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/dilas/ua/endpoint/StudentEndpoint.java
@@ -1,12 +1,16 @@
 package br.com.dilas.ua.endpoint;
 
 import br.com.dilas.ua.error.CustomErrorType;
+import br.com.dilas.ua.error.ResourceNotFoundException;
 import br.com.dilas.ua.model.Student;
 import br.com.dilas.ua.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("students")
@@ -26,27 +30,37 @@ public class StudentEndpoint {
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<?> getStudentById(@PathVariable("id") Long id){
-    Student student = dao.findById(id).get();
-    if(student == null){//não achou
-        return new ResponseEntity<>(new CustomErrorType("no student found"), HttpStatus.NOT_FOUND);
+        verifyIfStudentExists(id);
+        return new ResponseEntity<>(dao.findById(id), HttpStatus.OK);
     }
-    return new ResponseEntity<>(student, HttpStatus.OK);
+
+    @GetMapping(path = "/findByName/{name}")
+    public ResponseEntity<?> getStudentByName(@PathVariable("name") String name){
+        return new ResponseEntity<>(dao.findByNameIgnoreCaseContaining(name), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<?> save(@RequestBody Student student){
+    public ResponseEntity<?> save(@Valid @RequestBody Student student){
         dao.save(student);
         return new ResponseEntity(student, HttpStatus.OK);
     }
 
     @DeleteMapping
     public ResponseEntity<?> delete(@RequestBody Student student){
+        verifyIfStudentExists(student.getId());
         dao.delete(student);
         return new ResponseEntity(HttpStatus.OK);
     }
     @PutMapping
     public ResponseEntity<?> update(@RequestBody Student student){
+        verifyIfStudentExists(student.getId());
         dao.save(student);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    private void verifyIfStudentExists(Long id){
+        if(!dao.findById(id).isPresent()){
+            throw new ResourceNotFoundException("Student not found for id: "+id);
+        }
     }
 }
--- src/main/java/br/com/dilas/ua/model/Student.java
+++ src/main/java/br/com/dilas/ua/model/Student.java
@@ -1,6 +1,8 @@
 package br.com.dilas.ua.model;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -9,10 +11,21 @@ import static java.util.Arrays.asList;
 
 @Entity
 public class Student extends AbstractEntity{
-
+    @NotEmpty
     private String name;
 
-    public Student( String name){
+    @Email
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Student(String name){
         this.name = name;
     }
     public Student(){
